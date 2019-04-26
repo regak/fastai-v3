dec34f97944cdf0940b41b54763945facfce338f
Alexander <regak2010@gmail.com>
2019-04-26 16:28:54 +0800

    New model

--- app/server.py
+++ app/server.py
@@ -10,7 +10,7 @@ from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1' original
 #export_file_url = 'https://drive.google.com/uc?export=download&id=1IcH9W_KY0XaHf2sR8iknVLKXjubddx6J' model-1
-export_file_url = 'https://drive.google.com/file/d/1W0GyHzpkpqdND2x_ZGLcbeZFucDiZHxe/view?usp=drivesdk' #model-2
+export_file_url = 'https://drive.google.com/uc?export=download&id=1W0GyHzpkpqdND2x_ZGLcbeZFucDiZHxe' #model-2
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
